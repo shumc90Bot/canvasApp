d67808e20b3394f07747996cf6a63f41d91c2b90
Ken Tsang <kentsang311@gmail.com>
2023-11-30 14:40:06 +0800

    New branch

---
+++ javascript/eraser.js
@@ -0,0 +1,16 @@
+/**********************************************
+ * Eraser Functionality
+ * ==================================
+ * This class extends the PaintFunction class, which you can find in canvas-common
+ ***********************************************/
+class Eraser extends PaintFunction {
+    // This class extends the PaintFunction class
+    constructor(contextReal) {
+        super();
+        this.contextReal = contextReal;
+    }
+
+    onMouseDown(cord, event) {
+        
+    }
+}
